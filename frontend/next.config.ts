994283cee094c6f53d17489e08d6f6beb9b12bc0
Pemith Wansitha <Wansitha1@gmail.com>
2026-04-03 15:56:47 +0530

    added the passenger newsfeed page

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "t4.ftcdn.net",
+    ],
+  },
 };
 
 export default nextConfig;
