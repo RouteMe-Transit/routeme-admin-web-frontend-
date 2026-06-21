0a6088decccefb3019cbd48f771c6b08552aa5b6
Chamodya Wijerathne <chamodyawijerathne096@gmail.com>
2026-06-21 23:37:12 +0530

    Minor fix on layout

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
 		<div className="relative min-h-screen">
 			<div className="absolute top-0 left-0 h-[87px] w-full  bg-[#183555] sticky" ><LogoNname/></div>
             
-			<div className="pt-16">{children}</div>
+			<div className="">{children}</div>
 		</div>
 	);
 }
